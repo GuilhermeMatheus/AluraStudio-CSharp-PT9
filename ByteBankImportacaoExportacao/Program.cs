ebf104741b82149d4b2a223a6b0a8ea9488e8283
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-08-06 17:07:35 -0300

    Video 3.1

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.IO; // IO = Input e Output
-
+using System.Globalization;
 
 namespace ByteBankImportacaoExportacao 
 { 
@@ -16,22 +16,45 @@ namespace ByteBankImportacaoExportacao
         {
             var enderecoDoArquivo = "contas.txt";
 
-            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open)) 
             using (var leitor = new StreamReader(fluxoDeArquivo))
-            {
-                while (!leitor.EndOfStream)
-                {
+            { 
+                while (!leitor.EndOfStream) 
+                { 
                     var linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
+                    var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                    var msg = $"Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}. Saldo: {contaCorrente.Saldo}";
+                    Console.WriteLine(msg);
+                    //Console.WriteLine(linha);
                 } 
             }
             
-
-
             Console.ReadLine();
         }
 
-        
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            string[] campos = linha.Split(' ');
+
+            var agencia = campos[0];
+            var numero = campos[1];
+            var saldo = campos[2].Replace('.', ',');
+            var nomeTitular = campos[3];
+
+            var agenciaComoInt = int.Parse(agencia);
+            var numeroComoInt = int.Parse(numero);
+            var saldoComoDouble = double.Parse(saldo);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
+            resultado.Depositar(saldoComoDouble);
+            resultado.Titular = titular;
+
+            return resultado;
+        }
 
     }
 } 
